1242653858cd695f14010382821f99d51ef10c80
Tamas Herman <hermantamas@gmail.com>
2011-10-20 01:21:54 +0700

    convert only todays and future events into the json database

--- tsv2json.r
+++ tsv2json.r
@@ -1,7 +1,7 @@
 REBOL [
-  Title: "Google Docs Tab Separated Values spreadsheet export to JSON converter"
-  Date: 2011-08-08
-  Author: onetom@hackerspace.sg
+	Title: "Google Docs Tab Separated Values spreadsheet export to JSON converter"
+	Date: 2011-08-08
+	Author: onetom@hackerspace.sg
 ]
 
 lines: read/lines %"11_13_OCT_17.tsv"
@@ -9,13 +9,15 @@ fields: parse/all lines/1 "^-"  remove lines
 
 print "events = ["
 foreach l lines [
-l: parse/all l "^-"
-print "{"
-for i 1 length? fields 1 [
-print rejoin [{"} fields/:i {": "}
-	replace/all trim to-string l/:i {"} {\"} {",}]
-]
-print "},"
+	l: parse/all l "^-"
+	if now/date <= to-date l/1 [
+		print "{"
+		for i 1 length? fields 1 [
+			print rejoin [{"} fields/:i {": "}
+				replace/all trim to-string l/:i {"} {\"} {",}]
+		]
+		print "},"
+	]
 ]
 print "]"
 
