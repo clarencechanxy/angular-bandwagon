27a66d142724dc15fb5da335be87f575a53cba97
clarencechanxy <clarencechanxy@gmail.com>
2011-10-23 03:06:59 +0800

    displaying 3 days of history

--- tsv2json.r
+++ tsv2json.r
@@ -4,13 +4,13 @@ Date: 2011-08-08
 Author: onetom@hackerspace.sg
 ]
 
-lines: read/lines %"11_13_OCT_17.tsv"
+lines: read/lines %"/Users/Clarence/Dropbox/Bandwagon_data/events.tsv"
 fields: parse/all lines/1 "^-" remove lines
 
 print "events = ["
 foreach l lines [
 l: parse/all l "^-"
-if now/date <= to-date l/1 [
+if now/date - 3 <= to-date l/1 [
 print "{"
 for i 1 length? fields 1 [
 print rejoin [{"} fields/:i {": "}
@@ -20,5 +20,7 @@ print "},"
 ]
 ]
 print "]"
+
+
 
 
