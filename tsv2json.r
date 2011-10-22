463558852195d138bb6ef2d44e001340d66849b3
Tamas Herman <hermantamas@gmail.com>
2011-10-22 15:13:52 +0700

    still keep past 7 days of events

--- tsv2json.r
+++ tsv2json.r
@@ -1,24 +1,22 @@
 REBOL [
-Title: "Google Docs Tab Separated Values spreadsheet export to JSON converter"
-Date: 2011-08-08
-Author: onetom@hackerspace.sg
+	Title: "Google Docs Tab Separated Values spreadsheet export to JSON converter"
+	Date: 2011-08-08
+	Author: onetom@hackerspace.sg
 ]
 
 lines: read/lines %"11_13_OCT_17.tsv"
-fields: parse/all lines/1 "^-" remove lines
+fields: parse/all lines/1 "^-"  remove lines
 
 print "events = ["
 foreach l lines [
-l: parse/all l "^-"
-if now/date <= to-date l/1 [
-print "{"
-for i 1 length? fields 1 [
-print rejoin [{"} fields/:i {": "}
-replace/all trim to-string l/:i {"} {\"} {",}]
-]
-print "},"
-]
+	l: parse/all l "^-"
+	if now/date - 7 <= to-date l/1 [
+		print "{"
+		for i 1 length? fields 1 [
+			print rejoin [{"} fields/:i {": "}
+				replace/all trim to-string l/:i {"} {\"} {",}]
+		]
+		print "},"
+	]
 ]
 print "]"
-
-
