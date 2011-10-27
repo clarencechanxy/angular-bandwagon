13c9659bdf37c7c393a4004531cee56c84340894
kimchoy <kiumjoon@gmail.com>
2011-10-28 00:14:34 +0800

    refactored bandwagon.js

--- app/js/bandwagon.js
+++ app/js/bandwagon.js
@@ -2,84 +2,59 @@
    info: kumjin.choy@gmail.com */   
 
 function getCurrentPath() {
-  var path = document.location.href.split('#/')[1]; 
-  return path;
+  return document.location.href.split('#/')[1];
 }
 
-function showCalendarToday() {
+dayNames = ["SUNDAY","MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+month = ["JANUARY", "FEBRUARY", "MARCH",  "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+short_month = ["Jan", "Feb", "Mar",  "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+function showToday() {
   today = new Date();
-  d = today.getDate();
-  dn = today.getDay();
-  m = today.getMonth();
-  y = today.getFullYear();
-  
-  dayNames = new Array("SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY");    
-  month = new Array("JANUARY", "FEBRUARY", "MARCH",  "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER");
-  short_month = new Array("Jan", "Feb", "Mar",  "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec");  
-  var current_day = d;
-  var day_name = dayNames[dn];
-  var current_month = month[m];
-  
-  $(".day_name").html(day_name);
-  $(".date_of_mth").html(current_day);
-  $(".month").html(current_month);
-
-  $("#calendar").data("date", d);
-  $("#calendar").data("month", m+1);
-  $("#calendar").data("year", y);
-  
-  // filter by date by default
-  $("#date_query").val(current_day + " " + short_month[m] + " " + y.toString().slice(2)).change();
+  computeCalendar();
 }
 
-function computeCalendar(nextOrPrev) {
+function computeCalendar() {
   // get current date first
   var current_d = $("#calendar").data("date");
   var current_m = $("#calendar").data("month");
   var current_y = $("#calendar").data("year");
-  
-  // init a new date
-  var current_date = new Date(current_m + " " + current_d+',' + current_y);
-  
-  // compute a next or previous day
-  new_day = today;
-  nextOrPrev();
-  
   // save it to data attributes and draw calendar again
-  var new_day_d = new_day.getDate();
-  var new_day_m = new_day.getMonth()+1;
-  var new_day_y = new_day.getFullYear();
-  $("#calendar").data("date", new_day_d);
-  $("#calendar").data("month", new_day_m);
-  $("#calendar").data("year", new_day_y);
+  var new_day_d = today.getDate();
+  var new_day_m = today.getMonth()+1;
+  var new_day_y = today.getFullYear();
+  $("#calendar").data("date", new_day_d)
+                .data("month", new_day_m)
+                .data("year", new_day_y);
   
   $(".date_of_mth").html(new_day_d);
-  $(".day_name").html(dayNames[new_day.getDay()]);
+  $(".day_name").html(dayNames[today.getDay()]);
   $(".month").html(month[new_day_m-1]);
   
-  $("#date_query").val(new_day_d + " " + (short_month[new_day_m-1]) + " " + new_day_y.toString().slice(2)).change(); 
+  $("#date_query").val(new_day_d + " " + (short_month[new_day_m-1]) + " " + (new_day_y % 100)).change(); 
 }
 
 function nextDay() {
-  new_day.setDate(today.getDate()+1);
+  today.setDate(today.getDate()+1);
+  computeCalendar();
 }
 
 function prevDay() {
-  new_day.setDate(today.getDate()-1);
+  today.setDate(today.getDate()-1);
+  computeCalendar();
 }
 
 // Calendar
-showCalendarToday();
+showToday();
   
-$("#calendar .new_day").click(function() {
-  computeCalendar(nextDay);
+$("#calendar .new_day").click(function(e) {
+  nextDay();
 });
 
 $("#calendar .prev_day").click(function() {
-  computeCalendar(prevDay);
+  prevDay();
 });
 
 $("#calendar .today").click(function() {
-  showCalendarToday();
+  showToday();
 });
-
